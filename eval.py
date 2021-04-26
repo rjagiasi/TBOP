8f5f31d8cfcfd3adcb63ba5eaaafe3a251d09533
spoorthykanduri <spoorthy.kanduri@tamu.edu>
2021-04-26 02:14:12 -0500

    implemented 5 min skip

--- eval.py
+++ eval.py
@@ -7,7 +7,7 @@ sheet_obj = wb_obj.active
 max_col = sheet_obj.max_column
 
 #m_row = sheet_obj.max_row
-m_row = 16
+m_row = 32
 excel_data_arr = []
 
 for i in range(2, m_row + 1):
@@ -22,9 +22,10 @@ with open('Data/Output/output.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
     for row in csv_reader:
+        key = row[1]
         value = row[2]
         value = int(value)
-        actual_data_arr.append(value)
+        actual_data_arr.append([key,value])
         
 
 # actual_data_arr = actual_data_arr[1:]
@@ -36,11 +37,19 @@ print("excel_data_arr",excel_data_arr)
 
 arr_size = len(actual_data_arr)
 count = 0
+correct = 0
 
 for i in range(1,arr_size):
-    if(actual_data_arr[i] == excel_data_arr[i]):
+    # print(int(int(actual_data_arr[i][0].split(':')[1])/5)%2)
+    if (int(int(actual_data_arr[i][1].split(':')[1])/5))%2 == 1:
+        print(i)
+        continue 
+    if(actual_data_arr[i][1] == excel_data_arr[i]):
         count+=1
+        correct += 1
+    else:
+        count +=1
 
-percentage_accuracy = (count/arr_size)*100
+percentage_accuracy = (correct/count)*100
 
 print("accuracy of the code is :", percentage_accuracy)
